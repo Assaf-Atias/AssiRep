9e67d3fef2cc227c121cf7c681d89009704e4c2a
Assaf Atias <assaf.atias@qnomy.com>
2020-02-05 18:00:31 +0200

    Added Notes for git commands

---
+++ gitCommands.js
@@ -0,0 +1,14 @@
+// Git commands
+
+// GitBash - a linux command line for using git
+
+// $git init - Initialize Local Git Repository
+// $git add <file> - Add File(s) to Index
+// $git init - Check Status Of Working Tree
+// $git init - Commit Changes In Index
+// $git push - Push To Remote Repository
+// $git pull - Pull Latest From Remote Repository
+// $git clone - Clone Repository Into A New Directory
+
+
+
