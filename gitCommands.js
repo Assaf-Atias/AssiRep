2aa1279c4dc9b13a9271c8b65cc7364251c0fb1b
Assaf Atias <assaf.atias@qnomy.com>
2020-02-05 18:23:23 +0200

    Added some files

--- gitCommands.js
+++ gitCommands.js
@@ -10,5 +10,15 @@
 // $git pull - Pull Latest From Remote Repository
 // $git clone - Clone Repository Into A New Directory
 
+// git config --global user.name 'Assaf Atias'
+// git config --global user.email 'assaf.atias@qnomy.com'
 
 
+// add / commit / remove files
+// git status - let you see the status of the files
+// git add - add a file to the repository
+// git add . - add all files to the repository
+// git commit - commit a changeset
+// git commit -m '<comment>' - commit a changeset with comment, no need to save
+// git ignore - ignore files that are listed in 
+// the git.ignore file and won't add them to the commit
